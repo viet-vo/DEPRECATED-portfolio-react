3ce647b27167f68cf9ef26c6146acc9b72029fb7
Viet Vo <viet.allen.qvo@gmail.com>
2019-03-08 05:17:34 -0800

    init commit

--- src/components/Portfolio/Portfolio.js
+++
@@ -1,6 +0,0 @@
-import React from "react";
-import "./Portfolio.css"
-const Portfolio = props =>
-  <div className={`row${props.fluid ? "-fluid" : ""}`} {...props} />;
-
-export default Portfolio;
--- src/components/Row/Row.js
+++
@@ -1,6 +0,0 @@
-import React from "react";
-import "./Row.css"
-const Row = props =>
-  <div className={`row${props.fluid ? "-fluid" : ""}`} {...props} />;
-
-export default Row;
